7108b2214e648a751f015fff29a251236e105e3b
matti8702 <matteo7.gatti@mail.polimi.it>
2024-03-17 17:03:14 +0100

    Add documentation in CardColor and Side

--- src/main/java/it/polimi/ingsw/am01/model/card/CardColor.java
+++ src/main/java/it/polimi/ingsw/am01/model/card/CardColor.java
@@ -1,9 +1,15 @@
 package it.polimi.ingsw.am01.model.card;
 
+/**
+ * CardColor represents all possible colors of placeable card.
+ */
 public enum CardColor {
     RED,
     GREEN,
     BLUE,
     PURPLE,
+    /**
+     * NEUTRAL is the starter card's color
+     */
     NEUTRAL
 }
--- src/main/java/it/polimi/ingsw/am01/model/card/Side.java
+++ src/main/java/it/polimi/ingsw/am01/model/card/Side.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.am01.model.card;
 
+/**
+ * Side represents the two opposite faces of a card
+ */
 public enum Side {
     FRONT,
     BACK
