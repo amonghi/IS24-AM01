11b77ca2689aa2900f9e4cd532a72b5dcb50400d
Matteo Garzone <matteo.garzone@mail.polimi.it>
2024-03-18 17:45:44 +0100

    implementation of Deck

--- src/main/java/it/polimi/ingsw/am01/model/game/Deck.java
+++ src/main/java/it/polimi/ingsw/am01/model/game/Deck.java
@@ -2,25 +2,52 @@ package it.polimi.ingsw.am01.model.game;
 
 import it.polimi.ingsw.am01.model.card.Card;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Represents an ordered deck of Cards
+ */
 public class Deck implements DrawSource {
-    private List<Card> cards;
+    private final List<Card> cards;
 
-    public Deck(Iterable<Card> cards) {
-        throw new UnsupportedOperationException("TODO");
+    /**
+     * Constructs a new Deck from a list of Cards
+     *
+     * @param cards A reference to the list of cards from which the deck is composed
+     */
+
+    public Deck(List<Card> cards) {
+        this.cards = new ArrayList<>(cards);
     }
 
+    /**
+     * Shuffle the cards inside the Deck
+     */
     public void shuffle() {
-        throw new UnsupportedOperationException("TODO");
+        Collections.shuffle(cards);
     }
 
+    /**
+     * @return Returns whether the Deck is empty
+     */
     public boolean isEmpty() {
-        throw new UnsupportedOperationException("TODO");
+        return cards.isEmpty();
     }
 
+    /**
+     * Draws the last Card removing it from the Deck
+     *
+     * @return Returns the drawn card
+     * If the deck is empty, it returns {@code Optional.empty()}
+     */
     public Optional<Card> draw() {
-        throw new UnsupportedOperationException("TODO");
+        Card element = null;
+        if (!cards.isEmpty()) {
+            element = cards.removeLast();
+        }
+        return Optional.ofNullable(element);
     }
 }
