289aacd33f6593972fa5f3b88c29de7fffc88a64
Matteo Garzone <matteo.garzone@mail.polimi.it>
2024-03-21 12:03:23 +0100

    add PlayerProfile equals

--- src/main/java/it/polimi/ingsw/am01/model/player/PlayerProfile.java
+++ src/main/java/it/polimi/ingsw/am01/model/player/PlayerProfile.java
@@ -30,4 +30,19 @@ public class PlayerProfile {
     public boolean isConnected() {
         throw new UnsupportedOperationException("TODO");
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PlayerProfile that = (PlayerProfile) o;
+
+        return name.equals(that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
 }
