0037ab8d776370b080661a9d514b7332b85d6f05
Matteo Garzone <matteo.garzone@mail.polimi.it>
2024-03-24 22:05:44 +0100

    add gson in dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module it.polimi.ingsw.am01 {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires com.google.gson;
 
     opens it.polimi.ingsw.am01 to javafx.fxml;
     exports it.polimi.ingsw.am01;
