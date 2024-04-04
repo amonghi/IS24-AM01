9f8b9c285f40480a2f1b2dca02366109b779cce8
Matteo Gatti <matteo7.gatti@mail.polimi.it>
2024-04-04 18:15:41 +0200

    fix: deck tests

--- src/test/java/it/polimi/ingsw/am01/model/game/DeckTest.java
+++ src/test/java/it/polimi/ingsw/am01/model/game/DeckTest.java
@@ -6,11 +6,10 @@ import it.polimi.ingsw.am01.model.card.face.BackCardFace;
 import it.polimi.ingsw.am01.model.card.face.FrontCardFace;
 import it.polimi.ingsw.am01.model.card.face.corner.Corner;
 import it.polimi.ingsw.am01.model.collectible.Resource;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -34,100 +33,10 @@ class DeckTest {
                     Map.of(Resource.FUNGI, 1)
             )
     );
-    List<Card> cardList = List.of(
-            new Card(
-                    2,
-                    CardColor.RED,
-                    true,
-                    false,
-                    new FrontCardFace(
-                            Corner.empty(),
-                            Corner.filled(Resource.FUNGI),
-                            Corner.missing(),
-                            Corner.filled(Resource.FUNGI)
-                    ),
-                    new BackCardFace(
-                            Corner.empty(),
-                            Corner.empty(),
-                            Corner.empty(),
-                            Corner.empty(),
-                            Map.of(Resource.FUNGI, 1)
-                    )
-            ),
-            new Card(
-                    3,
-                    CardColor.BLUE,
-                    true,
-                    false,
-                    new FrontCardFace(
-                            Corner.empty(),
-                            Corner.filled(Resource.FUNGI),
-                            Corner.missing(),
-                            Corner.filled(Resource.FUNGI)
-                    ),
-                    new BackCardFace(
-                            Corner.empty(),
-                            Corner.empty(),
-                            Corner.empty(),
-                            Corner.empty(),
-                            Map.of(Resource.ANIMAL, 1)
-                    )
-            ),
-            new Card(
-                    4,
-                    CardColor.PURPLE,
-                    true,
-                    false,
-                    new FrontCardFace(
-                            Corner.empty(),
-                            Corner.filled(Resource.FUNGI),
-                            Corner.missing(),
-                            Corner.filled(Resource.FUNGI)
-                    ),
-                    new BackCardFace(
-                            Corner.empty(),
-                            Corner.empty(),
-                            Corner.empty(),
-                            Corner.empty(),
-                            Map.of(Resource.INSECT, 1)
-                    )
-            ),
-            new Card(
-                    5,
-                    CardColor.GREEN,
-                    true,
-                    false,
-                    new FrontCardFace(
-                            Corner.empty(),
-                            Corner.filled(Resource.FUNGI),
-                            Corner.missing(),
-                            Corner.filled(Resource.FUNGI)
-                    ),
-                    new BackCardFace(
-                            Corner.empty(),
-                            Corner.empty(),
-                            Corner.empty(),
-                            Corner.empty(),
-                            Map.of(Resource.PLANT, 1)
-                    )
-            )
-
-    );
-
-    @RepeatedTest(10)
-    void canShuffle() {
-        Deck deck = new Deck(cardList);
-        deck.shuffle();
-        List<Card> list = Stream.generate(deck::draw)
-                .takeWhile(Optional::isPresent)
-                .flatMap(Optional::stream)
-                .toList();
-        assertNotEquals(cardList, list);
-    }
 
     @Test
     void canCheckIfEmpty() {
-        Deck deck = new Deck(List.of(aCard));
+        Deck deck = new Deck(GameAssets.getInstance().getStarterCards().stream().limit(1).collect(Collectors.toList()));
         assertFalse(deck.isEmpty());
         deck.draw();
         assertTrue(deck.isEmpty());
