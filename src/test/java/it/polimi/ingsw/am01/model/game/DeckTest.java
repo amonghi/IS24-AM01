f09a7d548448f4a5b1cc8c36f853e062b909bfbf
Matteo Garzone <matteo.garzone@mail.polimi.it>
2024-04-07 19:38:48 +0200

    fix: add missing import

--- src/test/java/it/polimi/ingsw/am01/model/game/DeckTest.java
+++ src/test/java/it/polimi/ingsw/am01/model/game/DeckTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
