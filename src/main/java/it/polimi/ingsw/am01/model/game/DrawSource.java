26a1550fc2c24999c6c92d30f072210a4a170a33
Matteo Gatti <matteo7.gatti@mail.polimi.it>
2024-04-08 17:04:00 +0200

    docs: add javadoc of DrawSource

--- src/main/java/it/polimi/ingsw/am01/model/game/DrawSource.java
+++ src/main/java/it/polimi/ingsw/am01/model/game/DrawSource.java
@@ -4,6 +4,17 @@ import it.polimi.ingsw.am01.model.card.Card;
 
 import java.util.Optional;
 
+/**
+ * DrawSource represents a source from where drawing cards.
+ * There are two different draw source: {@link Deck} and {@link FaceUpCard}
+ * @see it.polimi.ingsw.am01.model.game.Deck
+ * @see it.polimi.ingsw.am01.model.game.FaceUpCard
+ */
 public interface DrawSource {
+    /**
+     * This method permits to draw a card
+     * @return {@code Optional.empty()} if source is empty, otherwise the first card of source.
+     * Implementations of this method are defined in {@link Deck} and {@link FaceUpCard}
+     */
     Optional<Card> draw();
 }
