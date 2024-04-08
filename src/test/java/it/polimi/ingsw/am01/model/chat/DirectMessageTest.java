9dce2ef2a55b600501c94ae3fdd6967e34389c4e
Matteo Gatti <matteo7.gatti@mail.polimi.it>
2024-04-08 22:22:05 +0200

    test: add test to DirectMessage

---
+++ src/test/java/it/polimi/ingsw/am01/model/chat/DirectMessageTest.java
@@ -0,0 +1,25 @@
+package it.polimi.ingsw.am01.model.chat;
+
+import it.polimi.ingsw.am01.model.player.PlayerProfile;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DirectMessageTest {
+
+    @Test
+    public void testDirectMessage() {
+        PlayerProfile sender = new PlayerProfile("Darius");
+        PlayerProfile otherPlayer = new PlayerProfile("George");
+        PlayerProfile recipient = new PlayerProfile("Mattew");
+        String content = "Hi Mattew, I'm Darius!";
+        DirectMessage directMessage = new DirectMessage(sender, recipient, content);
+
+        assertEquals(sender, directMessage.getSender());
+        assertEquals(content, directMessage.getContent());
+
+        assertTrue(directMessage.isRecipient(recipient));
+        assertFalse(directMessage.isRecipient(otherPlayer));
+        assertFalse(directMessage.isRecipient(sender));
+    }
+}
