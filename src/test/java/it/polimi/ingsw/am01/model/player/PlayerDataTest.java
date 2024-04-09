f46d1ff86ffbeef1b73cfc782bbeca23bd028f49
Matteo Gatti <matteo7.gatti@mail.polimi.it>
2024-04-09 11:16:16 +0200

    test: add test to PlayerData

---
+++ src/test/java/it/polimi/ingsw/am01/model/player/PlayerDataTest.java
@@ -0,0 +1,27 @@
+package it.polimi.ingsw.am01.model.player;
+
+import it.polimi.ingsw.am01.model.card.Card;
+import it.polimi.ingsw.am01.model.game.GameAssets;
+import it.polimi.ingsw.am01.model.objective.Objective;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PlayerDataTest {
+
+    @Test
+    public void testPlayerData() {
+        List<Card> hand = new ArrayList<>();
+        Objective objective = GameAssets.getInstance().getObjectives().getFirst();
+        PlayerColor playerColor = PlayerColor.YELLOW;
+
+        PlayerData playerData = new PlayerData(hand, objective, playerColor);
+
+        assertEquals(hand, playerData.getHand());
+        assertEquals(objective, playerData.getObjectiveChoice());
+        assertEquals(playerColor, playerData.getColorChoice());
+    }
+}
