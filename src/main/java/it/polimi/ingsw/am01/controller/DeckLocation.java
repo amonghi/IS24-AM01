58fc114b0e885eae1cb660419b7b5770fa01d39c
Dario Crosa <dario.crosa@mail.polimi.it>
2024-04-14 14:14:58 +0200

    docs(Controller): javadocs for DeckLocation

--- src/main/java/it/polimi/ingsw/am01/controller/DeckLocation.java
+++ src/main/java/it/polimi/ingsw/am01/controller/DeckLocation.java
@@ -1,6 +1,18 @@
 package it.polimi.ingsw.am01.controller;
 
+import it.polimi.ingsw.am01.model.game.Board;
+
+/**
+ * Represents the two possible decks from which the player can draw a card
+ */
 public enum DeckLocation {
+    /**
+     * @see Board#getResourceCardDeck()
+     */
     RESOURCE_CARD_DECK,
+
+    /**
+     * @see Board#getGoldenCardDeck()
+     */
     GOLDEN_CARD_DECK,
 }
