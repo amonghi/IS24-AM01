96b63b78a4832d4aebfa0d29c5beb425da7278d3
Dario Crosa <dario.crosa@mail.polimi.it>
2024-04-14 14:35:56 +0200

    test(PlayerManager): basic tests + missing @throws in javadocs

---
+++ src/test/java/it/polimi/ingsw/am01/model/player/PlayerManagerTest.java
@@ -0,0 +1,50 @@
+package it.polimi.ingsw.am01.model.player;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerManagerTest {
+
+    @Test
+    void profileCreation() {
+        PlayerManager pm = new PlayerManager();
+        assertFalse(pm.getProfile("Alice").isPresent());
+
+        PlayerProfile aProfile = pm.createProfile("Alice");
+        assertEquals("Alice", aProfile.getName());
+
+        assertTrue(pm.getProfile("Alice").isPresent());
+    }
+
+    @Test
+    void cannotCreateSecondProfileWithSameName() {
+        PlayerManager pm = new PlayerManager();
+        pm.createProfile("Alice");
+        assertTrue(pm.getProfile("Alice").isPresent());
+
+        assertThrows(IllegalStateException.class, () -> pm.createProfile("Alice"));
+    }
+
+    @Test
+    void profileDeletion() {
+        PlayerManager pm = new PlayerManager();
+        PlayerProfile aProfile = pm.createProfile("Alice");
+        assertTrue(pm.getProfile("Alice").isPresent());
+
+        pm.removeProfile(aProfile);
+        assertFalse(pm.getProfile("Alice").isPresent());
+    }
+
+    @Test
+    void cannotDeleteTwice() {
+        PlayerManager pm = new PlayerManager();
+        PlayerProfile aProfile = pm.createProfile("Alice");
+        pm.removeProfile(aProfile);
+        assertFalse(pm.getProfile("Alice").isPresent());
+
+        assertThrows(NoSuchElementException.class, () -> pm.removeProfile(aProfile));
+    }
+}
