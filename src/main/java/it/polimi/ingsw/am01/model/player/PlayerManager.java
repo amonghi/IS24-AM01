fb578f46c0c7b23c19413d0c46eb650ed31e1a7e
Dario Crosa <dario.crosa@mail.polimi.it>
2024-04-14 14:09:13 +0200

    docs(PlayerManager): add javadocs to public methods

--- src/main/java/it/polimi/ingsw/am01/model/player/PlayerManager.java
+++ src/main/java/it/polimi/ingsw/am01/model/player/PlayerManager.java
@@ -12,6 +12,13 @@ public class PlayerManager {
         profiles = new HashMap<>();
     }
 
+    /**
+     * Create a new profile and save it in this {@link PlayerManager}
+     *
+     * @param name The name of the player
+     * @return The created profile
+     * @throws IllegalStateException if a profile with that same name already exists
+     */
     public PlayerProfile createProfile(String name) {
         if (this.profiles.containsKey(name)) {
             throw new IllegalStateException("A player with that name already exists");
@@ -22,10 +29,19 @@ public class PlayerManager {
         return newProfile;
     }
 
+    /**
+     * @param name the name of the profile to get
+     * @return an {@link Optional} containing the {@link PlayerProfile} with that name if such a profile exists, {@link Optional#empty()} otherwise.
+     */
     public Optional<PlayerProfile> getProfile(String name) {
         return Optional.ofNullable(this.profiles.get(name));
     }
 
+    /**
+     * Unregister a profile when a player leaves
+     *
+     * @param profile the profile to un-register
+     */
     public void removeProfile(PlayerProfile profile) {
         if (!this.profiles.containsKey(profile.getName())) {
             throw new NoSuchElementException();
