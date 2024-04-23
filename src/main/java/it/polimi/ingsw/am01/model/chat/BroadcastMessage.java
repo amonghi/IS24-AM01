605793f70822de6d6a9bf47b19631d7804e48bd7
Matteo Garzone <matteo.garzone@mail.polimi.it>
2024-04-23 11:59:33 +0200

    feat: add toString, equals and hashCode to chat package

--- src/main/java/it/polimi/ingsw/am01/model/chat/BroadcastMessage.java
+++ src/main/java/it/polimi/ingsw/am01/model/chat/BroadcastMessage.java
@@ -2,15 +2,19 @@ package it.polimi.ingsw.am01.model.chat;
 
 import it.polimi.ingsw.am01.model.player.PlayerProfile;
 
+import java.util.Objects;
+
 /**
  * A {@code Message} for every {@link PlayerProfile} connected to the game
+ *
  * @see it.polimi.ingsw.am01.model.chat.Message
  */
-public class BroadcastMessage extends Message{
+public class BroadcastMessage extends Message {
 
     /**
      * Construct a new broadcast {@code Message}
-     * @param sender The {@link PlayerProfile} who send the {@code Message}
+     *
+     * @param sender  The {@link PlayerProfile} who send the {@code Message}
      * @param content The content of the {@code Message}
      */
     public BroadcastMessage(PlayerProfile sender, String content) {
@@ -18,7 +22,6 @@ public class BroadcastMessage extends Message{
     }
 
     /**
-     *
      * @param pp The {@link PlayerProfile} to determine if he or she is a recipient
      * @return Whether the specified {@link PlayerProfile} is a recipient or not.
      * The sender is not considered as a valid recipient
@@ -26,5 +29,37 @@ public class BroadcastMessage extends Message{
     @Override
     public boolean isRecipient(PlayerProfile pp) {
         return !pp.equals(super.getSender());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        return "DirectMessage{"
+                + "(" + super.getSender().getName() + "):"
+                + super.getContent()
+                + "}";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BroadcastMessage that = (BroadcastMessage) o;
+        return getSender().equals(that.getSender()) &&
+                getContent().equals(that.getContent());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(getSender(), getContent());
     }
 }
--- src/main/java/it/polimi/ingsw/am01/model/chat/DirectMessage.java
+++ src/main/java/it/polimi/ingsw/am01/model/chat/DirectMessage.java
@@ -2,8 +2,11 @@ package it.polimi.ingsw.am01.model.chat;
 
 import it.polimi.ingsw.am01.model.player.PlayerProfile;
 
+import java.util.Objects;
+
 /**
  * A {@code Message} for a specific {@link PlayerProfile} connected to the game
+ *
  * @see it.polimi.ingsw.am01.model.chat.Message
  */
 public class DirectMessage extends Message {
@@ -11,9 +14,10 @@ public class DirectMessage extends Message {
 
     /**
      * Construct a new direct message
-     * @param sender The {@link PlayerProfile} who send the {@code Message}
+     *
+     * @param sender    The {@link PlayerProfile} who send the {@code Message}
      * @param recipient The {@link PlayerProfile} who receive the {@code Message}
-     * @param content The content of the {@code Message}
+     * @param content   The content of the {@code Message}
      */
     public DirectMessage(PlayerProfile sender, PlayerProfile recipient, String content) {
         super(sender, content);
@@ -21,12 +25,44 @@ public class DirectMessage extends Message {
     }
 
     /**
-     *
      * @param pp The {@link PlayerProfile} to determine if he or she is a recipient
      * @return Whether the specified {@link PlayerProfile} is a recipient or not
      */
     @Override
     public boolean isRecipient(PlayerProfile pp) {
         return recipient.equals(pp);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        return "DirectMessage{"
+                + "(" + super.getSender().getName() + " -> " + recipient.getName() + "):"
+                + super.getContent()
+                + "}";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DirectMessage that = (DirectMessage) o;
+        return recipient.equals(that.recipient) &&
+                getSender().equals(that.getSender()) &&
+                getContent().equals(that.getContent());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(recipient, getSender(), getContent());
     }
 }
