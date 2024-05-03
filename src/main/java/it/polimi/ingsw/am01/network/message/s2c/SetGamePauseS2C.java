bbf1f277ee8b3db6757fd63d0aa663d4b41870ee
Matteo Gatti <matteo7.gatti@mail.polimi.it>
2024-05-03 21:42:59 +0200

    fix: add missing files

---
+++ src/main/java/it/polimi/ingsw/am01/network/message/s2c/SetGamePauseS2C.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.am01.network.message.s2c;
+
+import it.polimi.ingsw.am01.model.game.GameStatus;
+import it.polimi.ingsw.am01.network.message.S2CNetworkMessage;
+
+public class SetGamePauseS2C implements S2CNetworkMessage {
+    public static final String ID = "SET_PAUSE";
+    private final GameStatus gameStatus = GameStatus.SUSPENDED;
+
+    public SetGamePauseS2C() {
+    }
+
+    @Override
+    public String getId() {
+        return ID;
+    }
+
+    public GameStatus getGameStatus() {
+        return gameStatus;
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/am01/network/message/s2c/SetRecoverStatusS2C.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.am01.network.message.s2c;
+
+import it.polimi.ingsw.am01.model.game.GameStatus;
+import it.polimi.ingsw.am01.network.message.S2CNetworkMessage;
+
+public record SetRecoverStatusS2C(GameStatus recoverStatus) implements S2CNetworkMessage {
+    public static final String ID = "SET_RECOVER_STATUS";
+
+    @Override
+    public String getId() {
+        return ID;
+    }
+}
