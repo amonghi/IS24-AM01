8f3c5f066fa42694eebcfdaeba5df118662bb1ef
Matteo Gatti <matteo7.gatti@mail.polimi.it>
2024-05-03 20:51:28 +0200

    refactor(events): change input of send method

--- src/main/java/it/polimi/ingsw/am01/network/message/c2s/JoinGameC2S.java
+++ src/main/java/it/polimi/ingsw/am01/network/message/c2s/JoinGameC2S.java
@@ -2,7 +2,10 @@ package it.polimi.ingsw.am01.network.message.c2s;
 
 import it.polimi.ingsw.am01.controller.Controller;
 import it.polimi.ingsw.am01.controller.VirtualView;
-import it.polimi.ingsw.am01.model.exception.*;
+import it.polimi.ingsw.am01.model.exception.GameNotFoundException;
+import it.polimi.ingsw.am01.model.exception.IllegalGameStateException;
+import it.polimi.ingsw.am01.model.exception.IllegalMoveException;
+import it.polimi.ingsw.am01.model.exception.NotAuthenticatedException;
 import it.polimi.ingsw.am01.model.game.GameStatus;
 import it.polimi.ingsw.am01.network.Connection;
 import it.polimi.ingsw.am01.network.message.C2SNetworkMessage;
@@ -11,19 +14,17 @@ import it.polimi.ingsw.am01.network.message.s2c.GameAlreadyStartedS2C;
 import it.polimi.ingsw.am01.network.message.s2c.GameJoinedS2C;
 import it.polimi.ingsw.am01.network.message.s2c.GameNotFoundS2C;
 
-import java.util.Objects;
-
 public record JoinGameC2S(int gameId) implements C2SNetworkMessage {
     public static final String ID = "JOIN_GAME";
 
     @Override
     public void execute(Controller controller, Connection<S2CNetworkMessage, C2SNetworkMessage> connection, VirtualView virtualView) throws IllegalMoveException {
-        try{
+        try {
             controller.joinGame(gameId, virtualView.getPlayerProfile().orElseThrow(NotAuthenticatedException::new).getName());
             virtualView.setGame(virtualView.getGameManager().getGame(gameId).orElseThrow(() -> new GameNotFoundException(gameId)));
-            connection.send(new GameJoinedS2C(virtualView.getGame().orElseThrow().getId(), GameStatus.AWAITING_PLAYERS));
-        } catch(GameNotFoundException e){
-            connection.send(new GameNotFoundS2C(Objects.requireNonNull(virtualView.getGame().orElse(null)).getId()));
+            connection.send(new GameJoinedS2C(gameId, GameStatus.AWAITING_PLAYERS));
+        } catch (GameNotFoundException e) {
+            connection.send(new GameNotFoundS2C(gameId));
         } catch (IllegalGameStateException e) {
             connection.send(new GameAlreadyStartedS2C(gameId));
         }
