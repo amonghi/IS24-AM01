1e09b0266de76e91e4540634fbd6e09858e230d2
Dario Crosa <dario.crosa@mail.polimi.it>
2024-05-03 18:35:04 +0200

    refactor: better handling of possible exceptions in BaseTCPConnection

--- src/main/java/it/polimi/ingsw/am01/network/tcp/BaseTCPConnection.java
+++ src/main/java/it/polimi/ingsw/am01/network/tcp/BaseTCPConnection.java
@@ -2,14 +2,14 @@ package it.polimi.ingsw.am01.network.tcp;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParseException;
 import it.polimi.ingsw.am01.network.Connection;
+import it.polimi.ingsw.am01.network.ReceiveNetworkException;
 import it.polimi.ingsw.am01.network.message.NetworkMessage;
 import it.polimi.ingsw.am01.network.message.json.NetworkMessageTypeAdapterFactory;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
-import java.util.Scanner;
 
 public abstract class BaseTCPConnection<S extends NetworkMessage, R extends NetworkMessage> implements Connection<S, R> {
     private static final Gson gson = new GsonBuilder()
@@ -18,26 +18,36 @@ public abstract class BaseTCPConnection<S extends NetworkMessage, R extends Netw
     private final Class<S> sendType;
     private final Class<R> receiveType;
     protected final Socket socket;
-    private final Scanner in;
-    private final PrintWriter out;
+    private final BufferedReader in;
+    private final BufferedWriter out;
 
     public BaseTCPConnection(Class<S> sendType, Class<R> receiveType, Socket socket) throws IOException {
         this.sendType = sendType;
         this.receiveType = receiveType;
         this.socket = socket;
-        this.in = new Scanner(socket.getInputStream());
-        this.out = new PrintWriter(socket.getOutputStream(), true);
+        this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
     }
 
     @Override
     public void send(S message) {
-        String json = gson.toJson(message, sendType);
-        out.println(json);
+        try {
+            String json = gson.toJson(message, sendType);
+            out.write(json);
+            out.newLine();
+            out.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
-    public R receive() {
-        String json = in.nextLine();
-        return gson.fromJson(json, receiveType);
+    public R receive() throws ReceiveNetworkException {
+        try {
+            String json = in.readLine();
+            return gson.fromJson(json, receiveType);
+        } catch (IOException | JsonParseException e) {
+            throw new ReceiveNetworkException(e);
+        }
     }
 }
