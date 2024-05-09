bec106d871c883e795130916dbc6d41d7e610d3f
Dario Crosa <dario.crosa@mail.polimi.it>
2024-05-09 14:11:58 +0200

    feat: synchronize EventEmitterImpl

--- src/main/java/it/polimi/ingsw/am01/eventemitter/EventEmitterImpl.java
+++ src/main/java/it/polimi/ingsw/am01/eventemitter/EventEmitterImpl.java
@@ -10,10 +10,6 @@ import java.util.List;
  */
 public class EventEmitterImpl<E extends Event> implements EventEmitter<E> {
 
-    private interface RegistrationInternal<V extends Event> extends Registration {
-        void notifyIfInterested(V event);
-    }
-
     private final List<RegistrationInternal<E>> registrations;
 
     public EventEmitterImpl() {
@@ -24,7 +20,7 @@ public class EventEmitterImpl<E extends Event> implements EventEmitter<E> {
      * {@inheritDoc}
      */
     @Override
-    public EventEmitter.Registration onAny(EventListener<E> listener) {
+    public synchronized EventEmitter.Registration onAny(EventListener<E> listener) {
         RegistrationInternal<E> registration = listener::onEvent;
         this.registrations.add(registration);
         return registration;
@@ -34,7 +30,7 @@ public class EventEmitterImpl<E extends Event> implements EventEmitter<E> {
      * {@inheritDoc}
      */
     @Override
-    public <T extends E> Registration on(Class<T> eventClass, EventListener<T> listener) {
+    public synchronized <T extends E> Registration on(Class<T> eventClass, EventListener<T> listener) {
         RegistrationInternal<E> registration = event -> {
             if (eventClass.isInstance(event)) {
                 T castEvent = eventClass.cast(event);
@@ -49,7 +45,7 @@ public class EventEmitterImpl<E extends Event> implements EventEmitter<E> {
      * {@inheritDoc}
      */
     @Override
-    public boolean unregister(Registration registration) {
+    public synchronized boolean unregister(Registration registration) {
         //noinspection SuspiciousMethodCalls
         return this.registrations.remove(registration);
     }
@@ -60,7 +56,12 @@ public class EventEmitterImpl<E extends Event> implements EventEmitter<E> {
      * @param event the event to emit.
      */
     public void emit(E event) {
-        for (RegistrationInternal<E> registration : this.registrations) {
+        ArrayList<RegistrationInternal<E>> registrations;
+        synchronized (this) {
+            registrations = new ArrayList<>(this.registrations);
+        }
+
+        for (RegistrationInternal<E> registration : registrations) {
             registration.notifyIfInterested(event);
         }
     }
@@ -71,7 +72,11 @@ public class EventEmitterImpl<E extends Event> implements EventEmitter<E> {
      * @param source source of the events to re-emit.
      * @param <T>    the type of the events emitted from the source.
      */
-    public <T extends E> void bubble(EventEmitter<T> source) {
+    public synchronized  <T extends E> void bubble(EventEmitter<T> source) {
         source.onAny(this::emit);
+    }
+
+    private interface RegistrationInternal<V extends Event> extends Registration {
+        void notifyIfInterested(V event);
     }
 }
