1eaa1204c2fcc6f9faa63c68495b49473b1606e9
Matteo Gatti <matteo7.gatti@mail.polimi.it>
2024-05-13 11:24:23 +0200

    refactor: model events

--- src/main/java/it/polimi/ingsw/am01/model/event/AllPlayersJoinedEvent.java
+++ src/main/java/it/polimi/ingsw/am01/model/event/AllPlayersJoinedEvent.java
@@ -1,13 +1,4 @@
 package it.polimi.ingsw.am01.model.event;
 
-public class AllPlayersJoinedEvent implements GameEvent{
-    private int startingCardId;
-
-    public void setStartingCardId(int startingCardId) {
-        this.startingCardId = startingCardId;
-    }
-
-    public int getStartingCardId() {
-        return startingCardId;
-    }
+public record AllPlayersJoinedEvent() implements GameEvent {
 }
