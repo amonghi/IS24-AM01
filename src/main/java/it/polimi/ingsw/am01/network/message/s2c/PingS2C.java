0e8cf9c57b9f862f9bdbac68dd83ee45f858d48d
Alessandro Del Fatti <alessandro.delfatti@mail.polimi.it>
2024-05-13 16:18:19 +0200

    feat: add PingS2C network message to detect disconnection

---
+++ src/main/java/it/polimi/ingsw/am01/network/message/s2c/PingS2C.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.am01.network.message.s2c;
+
+import it.polimi.ingsw.am01.network.message.S2CNetworkMessage;
+
+public record PingS2C() implements S2CNetworkMessage {
+
+    public static final String ID = "PING";
+
+    @Override
+    public String getId() {
+        return ID;
+    }
+}
