bf5dd85c89df68352a4e7b7f23f4494e85980114
Dario Crosa <dario.crosa@mail.polimi.it>
2024-05-13 23:38:02 +0200

    feat: add Centered, Column layouts + CardComponent placeholder

--- src/main/java/it/polimi/ingsw/am01/tui/Main.java
+++ src/main/java/it/polimi/ingsw/am01/tui/Main.java
@@ -1,7 +1,13 @@
 package it.polimi.ingsw.am01.tui;
 
 import it.polimi.ingsw.am01.tui.component.Component;
-import it.polimi.ingsw.am01.tui.component.elements.*;
+import it.polimi.ingsw.am01.tui.component.elements.Border;
+import it.polimi.ingsw.am01.tui.component.elements.BorderStyle;
+import it.polimi.ingsw.am01.tui.component.elements.CardComponent;
+import it.polimi.ingsw.am01.tui.component.elements.Text;
+import it.polimi.ingsw.am01.tui.component.layout.Centered;
+import it.polimi.ingsw.am01.tui.component.layout.Column;
+import it.polimi.ingsw.am01.tui.component.layout.SpaceAround;
 import it.polimi.ingsw.am01.tui.component.layout.flex.FlexChild;
 import it.polimi.ingsw.am01.tui.component.layout.flex.FlexRow;
 import it.polimi.ingsw.am01.tui.rendering.Constraint;
@@ -23,30 +29,45 @@ public class Main {
                 while (true) {
                     // change the scree with time to show that it is adapting
                     time++;
-                    int screenWidth = 35 + (int) ((Math.sin((0.1 * (double) time)) + 1) * 20);
+                    int screenWidth = 100 + (int) ((Math.sin((0.1 * (double) time)) + 1) * 20);
 
                     // build screen
-                    Dimensions screenDimensions = new Dimensions(screenWidth, 10);
-                    CharBufferDrawArea screen = new CharBufferDrawArea(screenDimensions, '~');
+                    Dimensions screenDimensions = new Dimensions(screenWidth, 15);
+                    CharBufferDrawArea screen = new CharBufferDrawArea(screenDimensions, ' ');
 
                     Component component =
-                            new FlexRow(List.of(
-                                    new FlexChild.Flexible(1, new Container(1, BorderStyle.DEFAULT, 1, new Text("1/2 grow"))),
-                                    new FlexChild.Fixed(new Border(BorderStyle.DEFAULT, new Text("Fixed"))),
-                                    new FlexChild.Flexible(1, new Border(BorderStyle.DEFAULT, new Text("1/2 fixed"))),
-                                    new FlexChild.Fixed(new Rectangle(3, 3, 'X'))
-                            ));
+                            new Border(BorderStyle.DEFAULT,
+                                    Centered.vertically(
+                                            new Column(List.of(
+                                                    new FlexRow(List.of(
+                                                            new FlexChild.Flexible(1, Centered.horizontally(
+                                                                    new CardComponent()
+                                                            )),
+                                                            new FlexChild.Flexible(1, Centered.horizontally(
+                                                                    new CardComponent()
+                                                            ))
+                                                    )),
+                                                    new FlexRow(List.of(
+                                                            new FlexChild.Flexible(1, Centered.horizontally(
+                                                                    new SpaceAround(1, 1, 1, 1,
+                                                                            new Text("Screen width: " + screenWidth)
+                                                                    )
+                                                            ))
+                                                    ))
+                                            ))
+                                    )
+                            );
+
                     Sized sized = component.layout(Constraint.max(screenDimensions));
                     sized.placeAt(Position.ZERO).draw(screen);
 
                     // clear and print
                     System.out.print("\033[H\033[2J");
                     System.out.flush();
-                    System.out.println(screenWidth);
                     System.out.println(screen);
 
                     try {
-                        Thread.sleep(33);
+                         Thread.sleep(33);
                     } catch (InterruptedException e) {
                         return;
                     }
