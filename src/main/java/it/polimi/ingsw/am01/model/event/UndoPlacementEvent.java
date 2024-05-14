3bccc97797e9c62b0505abdd42e882c88f19fa28
Alessandro Del Fatti <alessandro.delfatti@mail.polimi.it>
2024-05-14 13:44:38 +0200

    feat: add score to UndoPlacementEvent and correspondent network message

--- src/main/java/it/polimi/ingsw/am01/model/event/UndoPlacementEvent.java
+++ src/main/java/it/polimi/ingsw/am01/model/event/UndoPlacementEvent.java
@@ -3,5 +3,5 @@ package it.polimi.ingsw.am01.model.event;
 import it.polimi.ingsw.am01.model.game.PlayArea;
 import it.polimi.ingsw.am01.model.player.PlayerProfile;
 
-public record UndoPlacementEvent(PlayerProfile pp, PlayArea.Position position, int seq) implements GameEvent {
+public record UndoPlacementEvent(PlayerProfile pp, PlayArea.Position position, int score, int seq) implements GameEvent {
 }
--- src/main/java/it/polimi/ingsw/am01/network/message/s2c/UpdatePlayAreaAfterUndoS2C.java
+++ src/main/java/it/polimi/ingsw/am01/network/message/s2c/UpdatePlayAreaAfterUndoS2C.java
@@ -2,7 +2,7 @@ package it.polimi.ingsw.am01.network.message.s2c;
 
 import it.polimi.ingsw.am01.network.message.S2CNetworkMessage;
 
-public record UpdatePlayAreaAfterUndoS2C(String profile, int i, int j, int seq) implements S2CNetworkMessage {
+public record UpdatePlayAreaAfterUndoS2C(String profile, int i, int j, int score, int seq) implements S2CNetworkMessage {
 
     public static final String ID = "UNDO_PLACEMENT";
 
