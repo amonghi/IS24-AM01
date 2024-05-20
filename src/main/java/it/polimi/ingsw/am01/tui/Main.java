cbf47faabdf96afce894d8cdac67b91c5a6b226b
Dario Crosa <dario.crosa@mail.polimi.it>
2024-05-20 20:17:00 +0200

    feat: adapt to terminal size and detect resizing

--- src/main/java/it/polimi/ingsw/am01/tui/Main.java
+++ src/main/java/it/polimi/ingsw/am01/tui/Main.java
@@ -1,8 +1,6 @@
 package it.polimi.ingsw.am01.tui;
 
 import it.polimi.ingsw.am01.tui.component.Component;
-import it.polimi.ingsw.am01.tui.component.elements.Border;
-import it.polimi.ingsw.am01.tui.component.elements.BorderStyle;
 import it.polimi.ingsw.am01.tui.component.elements.CardComponent;
 import it.polimi.ingsw.am01.tui.component.elements.Text;
 import it.polimi.ingsw.am01.tui.component.layout.Centered;
@@ -15,64 +13,86 @@ import it.polimi.ingsw.am01.tui.rendering.Dimensions;
 import it.polimi.ingsw.am01.tui.rendering.Position;
 import it.polimi.ingsw.am01.tui.rendering.Sized;
 import it.polimi.ingsw.am01.tui.rendering.draw.CharBufferDrawArea;
+import it.polimi.ingsw.am01.tui.terminal.ResizeEvent;
+import it.polimi.ingsw.am01.tui.terminal.Terminal;
+import it.polimi.ingsw.am01.tui.terminal.UnixTerminal;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Terminal unixTerminal = new UnixTerminal();
 
-        Thread thread = new Thread(new Runnable() {
-            int time = 0;
-
-            @Override
-            public void run() {
-                while (true) {
-                    // change the scree with time to show that it is adapting
-                    time++;
-                    int screenWidth = 100 + (int) ((Math.sin((0.05 * (double) time)) + 1) * 20);
-
-                    // build screen
-                    Dimensions screenDimensions = new Dimensions(screenWidth, 15);
-                    CharBufferDrawArea screen = new CharBufferDrawArea(screenDimensions, ' ');
-
-                    Component component =
-                            new Border(BorderStyle.DEFAULT,
-                                    Centered.vertically(
-                                            new Column(List.of(
-                                                    new FlexRow(List.of(
-                                                            new FlexChild.Flexible(1, Centered.horizontally(
-                                                                    new CardComponent()
-                                                            )),
-                                                            new FlexChild.Flexible(1, Centered.horizontally(
-                                                                    new CardComponent()
-                                                            ))
-                                                    )),
-                                                    new FlexRow(List.of(
-                                                            new FlexChild.Flexible(1, Centered.horizontally(
-                                                                    new SpaceAround(1, 1, 1, 1,
-                                                                            new Text("Screen width: " + screenWidth)
-                                                                    )
-                                                            ))
-                                                    ))
-                                            ))
-                                    )
-                            );
-
-                    Sized sized = component.layout(Constraint.max(screenDimensions));
-                    sized.placeAt(Position.ZERO).draw(screen);
-
-                    // clear and print
-                    System.out.print("\033[H\033[2J" + screen);
-                    System.out.flush();
-
-                    try {
-                         Thread.sleep(33);
-                    } catch (InterruptedException e) {
-                        return;
-                    }
-                }
-            }
+        unixTerminal.on(ResizeEvent.class, event -> {
+            draw(unixTerminal);
         });
-        thread.start();
+        draw(unixTerminal);
+
+        // leave the process hanging to stop the JVM from shutting down
+        (new Scanner(System.in)).next();
+    }
+
+    private static void draw(Terminal terminal) {
+        // clear
+        System.out.println("\033[H\033[2J");
+        System.out.flush();
+
+        // build screen
+        Dimensions screenDimensions = terminal.getDimensions();
+        Dimensions drawDimensions = Dimensions.of(screenDimensions.width(), screenDimensions.height() - 4);
+        CharBufferDrawArea screen = new CharBufferDrawArea(drawDimensions, ' ');
+
+        long t0 = System.nanoTime();
+
+        Component component =
+                Centered.vertically(
+                        new Column(List.of(
+                                new FlexRow(List.of(
+                                        new FlexChild.Flexible(1, Centered.horizontally(
+                                                new CardComponent()
+                                        )),
+                                        new FlexChild.Flexible(1, Centered.horizontally(
+                                                new CardComponent()
+                                        ))
+                                )),
+                                new FlexRow(List.of(
+                                        new FlexChild.Flexible(1, Centered.horizontally(
+                                                new SpaceAround(1, 1, 1, 1,
+                                                        new Text("Screen width: " + screenDimensions.width())
+                                                )
+                                        ))
+                                )),
+                                new FlexRow(List.of(
+                                        new FlexChild.Flexible(1, Centered.horizontally(
+                                                new SpaceAround(1, 1, 1, 1,
+                                                        new Text("Screen height: " + screenDimensions.height())
+                                                )
+                                        ))
+                                ))
+                        ))
+                );
+
+
+        long t1 = System.nanoTime();
+
+        Sized sized = component.layout(Constraint.max(drawDimensions));
+
+        long t2 = System.nanoTime();
+
+        sized.placeAt(Position.ZERO).draw(screen);
+
+        long t3 = System.nanoTime();
+
+        long build = (t1 - t0) / 1000;
+        long layout = (t2 - t1) / 1000;
+        long draw = (t3 - t2) / 1000;
+
+        // print
+        System.out.print(screen);
+        System.out.flush();
+        System.out.println("build: " + build + " ms");
+        System.out.println("layout: " + layout + " ms");
+        System.out.println("draw: " + draw + " ms");
     }
 }
---
+++ src/main/java/it/polimi/ingsw/am01/tui/terminal/ResizeEvent.java
@@ -0,0 +1,6 @@
+package it.polimi.ingsw.am01.tui.terminal;
+
+import it.polimi.ingsw.am01.eventemitter.Event;
+
+public record ResizeEvent() implements Event {
+}
