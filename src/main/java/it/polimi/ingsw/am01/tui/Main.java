1668e98c0d7c38f4c6b40e2185a2a2ff84eb65b3
Dario Crosa <dario.crosa@mail.polimi.it>
2024-05-22 16:24:07 +0200

    refactor: split rendering logic from application logic

--- src/main/java/it/polimi/ingsw/am01/tui/Main.java
+++ src/main/java/it/polimi/ingsw/am01/tui/Main.java
@@ -1,11 +1,14 @@
 package it.polimi.ingsw.am01.tui;
 
+import it.polimi.ingsw.am01.tui.terminal.Terminal;
+import it.polimi.ingsw.am01.tui.terminal.UnixTerminal;
+
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        TuiApplication app = new TuiApplication();
+        Terminal terminal = new UnixTerminal();
+        CodexNaturalisTuiApplication app = new CodexNaturalisTuiApplication(terminal);
         Runtime.getRuntime().addShutdownHook(new Thread(app::shutdown));
-        app.start();
     }
 }
--- src/main/java/it/polimi/ingsw/am01/tui/TuiApplication.java
+++ src/main/java/it/polimi/ingsw/am01/tui/TuiApplication.java
@@ -2,174 +2,99 @@ package it.polimi.ingsw.am01.tui;
 
 import it.polimi.ingsw.am01.eventemitter.EventEmitter;
 import it.polimi.ingsw.am01.tui.component.Component;
-import it.polimi.ingsw.am01.tui.component.elements.CardComponent;
-import it.polimi.ingsw.am01.tui.component.elements.Text;
-import it.polimi.ingsw.am01.tui.component.layout.Centered;
-import it.polimi.ingsw.am01.tui.component.layout.Column;
-import it.polimi.ingsw.am01.tui.component.layout.SpaceAround;
-import it.polimi.ingsw.am01.tui.component.layout.flex.FlexChild;
-import it.polimi.ingsw.am01.tui.component.layout.flex.FlexRow;
 import it.polimi.ingsw.am01.tui.rendering.Constraint;
 import it.polimi.ingsw.am01.tui.rendering.Dimensions;
 import it.polimi.ingsw.am01.tui.rendering.Position;
-import it.polimi.ingsw.am01.tui.rendering.Sized;
+import it.polimi.ingsw.am01.tui.rendering.SizedPositioned;
 import it.polimi.ingsw.am01.tui.rendering.draw.CharBufferDrawArea;
+import it.polimi.ingsw.am01.tui.rendering.draw.DrawArea;
 import it.polimi.ingsw.am01.tui.terminal.ResizeEvent;
 import it.polimi.ingsw.am01.tui.terminal.Terminal;
-import it.polimi.ingsw.am01.tui.terminal.UnixTerminal;
 
-import java.io.IOException;
-import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.function.Consumer;
 
-public class TuiApplication {
-    private static final char EXIT_CHAR = 'q';
-
+public abstract class TuiApplication<S extends TuiApplication.State> {
     private final Terminal terminal;
-    private final KeyboardReader keyboard;
-    private ExecutorService renderService;
+    private final S state;
+    private final ExecutorService renderService;
+    private final EventEmitter.Registration registration;
 
-    private String lastPressed;
-    private boolean alreadyShutDown;
-    private EventEmitter.Registration registration;
-
-    public TuiApplication() {
-        this.terminal = new UnixTerminal();
-        this.renderService = null;
-        this.alreadyShutDown = false;
-        this.keyboard = new KeyboardReader(System.in);
-    }
-
-    public void start() {
-        this.terminal.enableRawMode();
+    public TuiApplication(Terminal terminal, S initialState) {
+        this.terminal = terminal;
+        this.state = initialState;
         this.renderService = Executors.newSingleThreadExecutor();
-        this.renderService.submit(this::draw);
+
+        this.terminal.enableRawMode();
 
         this.registration = this.terminal.on(ResizeEvent.class, event -> {
-            renderService.submit(this::draw);
+            renderService.submit(this::render);
         });
 
-        Thread keyboardListenerThread = new Thread(() -> {
-            while (true) {
-                try {
-                    Key key = this.keyboard.nextSupportedKey();
-
-                    boolean quit = false;
-                    String keyString = switch (key) {
-                        case Key.Character(char c) -> {
-                            if (c == EXIT_CHAR) {
-                                quit = true;
-                            }
-                            yield String.valueOf(c);
-                        }
-                        case Key.Arrow(Key.Arrow.Direction dir) -> switch (dir) {
-                            case UP -> "⬆️";
-                            case DOWN -> "⬇️";
-                            case LEFT -> "⬅️";
-                            case RIGHT -> "➡️";
-                        };
-                    };
-
-                    if (quit) {
-                        this.shutdown();
-                        return;
-                    }
-
-                    renderService.submit(() -> {
-                        this.lastPressed = keyString;
-                        draw();
-                    });
-                } catch (IOException e) {
-                    this.shutdown();
-                    return;
-                }
-            }
-        });
-        keyboardListenerThread.start();
+        // first render
+        this.renderService.submit(this::render);
     }
 
     public void shutdown() {
-        if (this.alreadyShutDown) {
-            return;
-        }
-        this.alreadyShutDown = true;
-
-        if (this.registration != null) {
-            this.terminal.unregister(registration);
-        }
-        if (this.renderService != null) {
-            this.renderService.shutdown();
-        }
+        this.terminal.unregister(registration);
         this.terminal.disableRawMode();
+        this.renderService.shutdown();
     }
 
-    private void draw() {
-        // build screen
-        Dimensions screenDimensions = terminal.getDimensions();
-        Dimensions drawDimensions = Dimensions.of(screenDimensions.width(), screenDimensions.height() - 4);
-        CharBufferDrawArea screen = new CharBufferDrawArea(drawDimensions, ' ');
+    public void updateState(Consumer<S> stateUpdater) {
+        this.renderService.submit(() -> {
+            stateUpdater.accept(this.state);
+            this.render();
+        });
+    }
 
-        long t0 = System.nanoTime();
+    private void render() {
+        Dimensions terminalDimensions = this.terminal.getDimensions();
+        Dimensions drawDimensions = this.state.debugEnabled
+                ? terminalDimensions
+                : terminalDimensions.shrink(0, 3);
+        DrawArea drawArea = new CharBufferDrawArea(drawDimensions, ' ');
 
-        Component component =
-                Centered.vertically(
-                        new Column(List.of(
-                                new FlexRow(List.of(
-                                        new FlexChild.Flexible(1, Centered.horizontally(
-                                                new CardComponent()
-                                        )),
-                                        new FlexChild.Flexible(1, Centered.horizontally(
-                                                new CardComponent()
-                                        ))
-                                )),
-                                new FlexRow(List.of(
-                                        new FlexChild.Flexible(1, Centered.horizontally(
-                                                new SpaceAround(1, 1, 1, 1,
-                                                        new Text("Screen width: " + screenDimensions.width())
-                                                )
-                                        ))
-                                )),
-                                new FlexRow(List.of(
-                                        new FlexChild.Flexible(1, Centered.horizontally(
-                                                new SpaceAround(1, 1, 1, 1,
-                                                        new Text("Screen height: " + screenDimensions.height())
-                                                )
-                                        ))
-                                )),
-                                new FlexRow(List.of(
-                                        new FlexChild.Flexible(1, Centered.horizontally(
-                                                new SpaceAround(1, 1, 1, 1,
-                                                        new Text("Last pressed: " + lastPressed)
-                                                )
-                                        ))
-                                ))
-                        ))
-                );
+        long t0 = System.nanoTime(); // start time
 
-        long t1 = System.nanoTime();
+        Component rootComponent = this.compose(this.state);
 
-        Sized sized = component.layout(Constraint.max(drawDimensions));
+        long t1 = System.nanoTime(); // done composing, begin layout
 
-        long t2 = System.nanoTime();
+        SizedPositioned layoutRoot = rootComponent
+                .layout(Constraint.max(drawDimensions))
+                .placeAt(Position.ZERO);
 
-        sized.placeAt(Position.ZERO).draw(screen);
+        long t2 = System.nanoTime(); // done layout, begin drawing
 
-        long t3 = System.nanoTime();
+        layoutRoot.draw(drawArea);
+
+        long t3 = System.nanoTime(); // done drawing
 
         long build = (t1 - t0) / 1000;
         long layout = (t2 - t1) / 1000;
         long draw = (t3 - t2) / 1000;
 
         // print
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder
-                .append("\033[H\033[2J")
-                .append(screen)
-                .append("build: ").append(build).append(" ms\r\n")
-                .append("layout: ").append(layout).append(" ms\r\n")
-                .append("draw: ").append(draw).append(" ms\r\n");
+        StringBuilder builder = new StringBuilder();
+        builder
+                .append("\033[H\033[2J") // clear screen and move cursor to 0,0
+                .append(drawArea);
 
-        System.out.println(stringBuilder);
+        if (this.state.debugEnabled) {
+            builder
+                    .append("build: ").append(build).append(" ms\r\n")
+                    .append("layout: ").append(layout).append(" ms\r\n")
+                    .append("draw: ").append(draw).append(" ms\r\n");
+        }
+
+        System.out.println(builder);
+    }
+
+    abstract Component compose(S state);
+
+    public static class State {
+        public boolean debugEnabled = false;
     }
 }
