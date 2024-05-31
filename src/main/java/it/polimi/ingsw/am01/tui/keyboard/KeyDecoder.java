c767450735980967973de106005a10c925d7eabc
Dario Crosa <dario.crosa@mail.polimi.it>
2024-05-31 18:55:59 +0200

    feat: support for reading ALT key

--- src/main/java/it/polimi/ingsw/am01/tui/keyboard/KeyDecoder.java
+++ src/main/java/it/polimi/ingsw/am01/tui/keyboard/KeyDecoder.java
@@ -1,9 +1,10 @@
 package it.polimi.ingsw.am01.tui.keyboard;
 
+import it.polimi.ingsw.am01.tui.rendering.ansi.Ansi;
+
 import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Optional;
 
 public class KeyDecoder {
     private final InputStream inputStream;
@@ -12,90 +13,93 @@ public class KeyDecoder {
         this.inputStream = inputStream;
     }
 
-    public Key nextSupportedKey() throws IOException {
-        Optional<Key> k;
+    public Key nextKey() throws IOException {
+        Key k;
 
         do {
-            k = this.nextKey();
-        } while (k.isEmpty());
+            k = this.tryNextKey();
+        } while (k == null);
 
-        return k.get();
+        return k;
     }
 
-    public Optional<Key> nextKey() throws IOException {
+    private Key tryNextKey() throws IOException {
         char c1 = this.readChar();
 
         if (c1 == '\177') {
-            return Optional.of(new Key.Backspace());
+            return new Key.Backspace();
         }
 
         if (c1 == '\r') {
-            return Optional.of(new Key.Enter());
+            return new Key.Enter();
         }
 
         if (c1 == '\t') {
-            return Optional.of(new Key.Tab());
+            return new Key.Tab();
         }
 
-        if (c1 != '\033') {
-            boolean ctrl = false;
-            if (c1 <= 31) {
-                ctrl = true;
-                c1 = (char) (c1 + 96);
-            }
-
-            return Optional.of(new Key.Character(ctrl, c1));
+        if (c1 == Ansi.ESC) {
+            return parseEscapeSequence();
         }
 
+        if (c1 < 32) {
+            char c = (char) (c1 + 96);
+            return new Key.Ctrl(c);
+        }
+
+        // ascii printable characters are between 32 and 127
+        if (isPrintableAscii(c1)) {
+            return new Key.Character(c1);
+        }
+
+        return null;
+    }
+
+    private Key parseEscapeSequence() throws IOException {
         char c2 = this.readChar();
-        Key k = switch (c2) {
-            case '[' -> {
-                char c3 = this.readChar();
+        if (c2 != '[') {
+            return isPrintableAscii(c2)
+                    ? new Key.Alt(c2)
+                    : null;
+        }
+
+        char c3 = this.readChar();
+        return switch (c3) {
+            case 'A' -> new Key.Arrow(Key.Arrow.Direction.UP);
+            case 'B' -> new Key.Arrow(Key.Arrow.Direction.DOWN);
+            case 'C' -> new Key.Arrow(Key.Arrow.Direction.RIGHT);
+            case 'D' -> new Key.Arrow(Key.Arrow.Direction.LEFT);
+            case 'H' -> new Key.Home();
+            case 'F' -> new Key.End();
+
+            case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' -> {
+                int c4 = System.in.read();
+                if (c4 != '~') {
+                    yield null;
+                }
+
                 yield switch (c3) {
-                    case 'A' -> new Key.Arrow(Key.Arrow.Direction.UP);
-                    case 'B' -> new Key.Arrow(Key.Arrow.Direction.DOWN);
-                    case 'C' -> new Key.Arrow(Key.Arrow.Direction.RIGHT);
-                    case 'D' -> new Key.Arrow(Key.Arrow.Direction.LEFT);
-                    case 'H' -> new Key.Home();
-                    case 'F' -> new Key.End();
-
-                    case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' -> {
-                        int c4 = System.in.read();
-                        if (c4 != '~') {
-                            yield null;
-                        }
-
-                        yield switch (c3) {
-                            case '1', '7':
-                                yield new Key.Home();
-                            case '3':
-                                yield new Key.Del();
-                            case '4', '8':
-                                yield new Key.End();
-                            case '5':
-                                yield new Key.PageUp();
-                            case '6':
-                                yield new Key.PageDown();
-                            default:
-                                yield null;
-                        };
-                    }
-
-                    default -> null;
+                    case '1', '7':
+                        yield new Key.Home();
+                    case '3':
+                        yield new Key.Del();
+                    case '4', '8':
+                        yield new Key.End();
+                    case '5':
+                        yield new Key.PageUp();
+                    case '6':
+                        yield new Key.PageDown();
+                    default:
+                        yield null;
                 };
             }
-            case 'O' -> {
-                char c3 = this.readChar();
-                yield switch (c3) {
-                    case 'H' -> new Key.Home();
-                    case 'F' -> new Key.End();
-                    default -> null;
-                };
-            }
+
             default -> null;
         };
+    }
 
-        return Optional.ofNullable(k);
+    private boolean isPrintableAscii(char c) {
+        return c >= 32 && c <= 127;
     }
 
     private char readChar() throws IOException {
