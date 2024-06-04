b8bebc4a946d1a96c199c1f2823def9df6c5fc9a
Dario Crosa <dario.crosa@mail.polimi.it>
2024-06-04 11:34:31 +0200

    fix: change main class to fix runtime error

--- src/main/java/it/polimi/ingsw/am01/client/gui/GUIClient.java
+++ src/main/java/it/polimi/ingsw/am01/client/gui/GUIClient.java
@@ -4,11 +4,6 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class GUIClient extends Application {
-
-    public static void main(String[] args) {
-        launch();
-    }
-
     @Override
     public void start(Stage stage) {
         new GUIView(stage);
