e8f7436bbb01c829448c04d11d87bb522899de40
Dario Crosa <dario.crosa@mail.polimi.it>
2024-06-05 22:41:32 +0200

    feat: Windows terminal

--- src/main/java/it/polimi/ingsw/am01/client/tui/Main.java
+++ src/main/java/it/polimi/ingsw/am01/client/tui/Main.java
@@ -1,13 +1,16 @@
 package it.polimi.ingsw.am01.client.tui;
 
+import com.sun.jna.Platform;
 import it.polimi.ingsw.am01.client.tui.terminal.Terminal;
 import it.polimi.ingsw.am01.client.tui.terminal.UnixTerminal;
-
-import java.io.IOException;
+import it.polimi.ingsw.am01.client.tui.terminal.WindowsTerminal;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        Terminal terminal = new UnixTerminal();
+    public static void main(String[] args) {
+        Terminal terminal = Platform.isWindows()
+                ? new WindowsTerminal()
+                : new UnixTerminal();
+
         new CodexNaturalisTuiApplication(terminal);
     }
 }
