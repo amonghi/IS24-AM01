90ec33168640f0a6c4ab246abcabb74a07e85e08
Matteo Gatti <matteo7.gatti@mail.polimi.it>
2024-06-06 21:56:11 +0200

    feat: handle disconnections in View

--- src/main/java/it/polimi/ingsw/am01/client/gui/GUIView.java
+++ src/main/java/it/polimi/ingsw/am01/client/gui/GUIView.java
@@ -82,6 +82,12 @@ public class GUIView extends View {
             };
         };
 
+        if(newSceneController.equals(CONNECTION_CONTROLLER) && !currentSceneController.equals(CONNECTION_CONTROLLER)){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setContentText("Lost connection!");
+            alert.show();
+        }
+
         newSceneController.loadScene(this.stage, stage.getScene().getWidth(), stage.getScene().getHeight());
         currentSceneController = newSceneController;
 
