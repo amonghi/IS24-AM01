b74df9d340f660f1b7841b2ba0ff90ccb3c5857e
Dario Crosa <dario.crosa@mail.polimi.it>
2024-06-10 16:23:12 +0200

    feat: add Scroll component

---
+++ src/main/java/it/polimi/ingsw/am01/client/tui/component/layout/Scroll.java
@@ -0,0 +1,42 @@
+package it.polimi.ingsw.am01.client.tui.component.layout;
+
+import it.polimi.ingsw.am01.client.tui.component.Component;
+import it.polimi.ingsw.am01.client.tui.rendering.Constraint;
+import it.polimi.ingsw.am01.client.tui.rendering.Dimensions;
+import it.polimi.ingsw.am01.client.tui.rendering.Position;
+import it.polimi.ingsw.am01.client.tui.rendering.RenderingContext;
+import it.polimi.ingsw.am01.client.tui.rendering.draw.DrawArea;
+
+public class Scroll extends SingleChildLayout {
+    private static final Constraint MAX_CONSTRAINT = Constraint.max(Dimensions.of(Integer.MAX_VALUE, Integer.MAX_VALUE));
+
+    private final int xOff;
+    private final int yOff;
+
+    public Scroll(int xOff, int yOff, Component child) {
+        super(child);
+        this.xOff = xOff;
+        this.yOff = yOff;
+    }
+
+    @Override
+    public void layout(Constraint constraint) {
+        this.child().layout(MAX_CONSTRAINT);
+        this.setDimensions(constraint.max());
+    }
+
+    @Override
+    public void draw(RenderingContext ctx, DrawArea a) {
+        RenderingContext newCtx = new RenderingContext(
+                ctx.global(),
+
+                // this tells the component it is rendering at 0,0
+                // FIXME: cannot report real position because it could be negative or off-screen
+                new RenderingContext.Local(Position.ZERO)
+        );
+
+        DrawArea relativeArea = a.getRelativeArea(this.xOff, this.yOff, child().dimensions());
+        child().draw(newCtx, relativeArea);
+    }
+
+}
--- src/main/java/it/polimi/ingsw/am01/client/tui/rendering/draw/DrawArea.java
+++ src/main/java/it/polimi/ingsw/am01/client/tui/rendering/draw/DrawArea.java
@@ -6,42 +6,79 @@ import it.polimi.ingsw.am01.client.tui.rendering.ansi.GraphicalRendition;
 import it.polimi.ingsw.am01.client.tui.rendering.ansi.GraphicalRenditionProperty;
 
 import java.util.Objects;
+import java.util.Optional;
 
 public class DrawArea {
     private final DrawBuffer drawBuffer;
-    private final Position offset;
-    private final Dimensions dimensions;
+    private final Rectangle cropArea;
+    private final Rectangle area;
     private GraphicalRendition rendition;
 
-    public DrawArea(DrawBuffer drawBuffer, Position offset, Dimensions dimensions) {
+    public DrawArea(DrawBuffer drawBuffer) {
         Objects.requireNonNull(drawBuffer);
-        Objects.requireNonNull(offset);
-        Objects.requireNonNull(dimensions);
 
-        if (offset.x() + dimensions.width() > drawBuffer.dimensions().width()) {
-            throw new IllegalArgumentException("DrawArea out of bounds");
-        }
+        Rectangle bufferArea = new Rectangle(new Point(0, 0), drawBuffer.dimensions());
 
-        if (offset.y() + dimensions.height() > drawBuffer.dimensions().height()) {
-            throw new IllegalArgumentException("DrawArea window out of bounds");
+        this.drawBuffer = drawBuffer;
+        this.cropArea = bufferArea;
+        this.area = bufferArea;
+        this.rendition = GraphicalRendition.DEFAULT;
+    }
+
+    protected DrawArea(DrawBuffer drawBuffer, Rectangle cropArea, Rectangle area) {
+        Rectangle bufferArea = new Rectangle(new Point(0, 0), drawBuffer.dimensions());
+        if (!bufferArea.contains(cropArea)) {
+            throw new IllegalArgumentException("crop area not inside drawBuffer area");
         }
 
         this.drawBuffer = drawBuffer;
-        this.offset = offset;
-        this.dimensions = dimensions;
+        this.cropArea = cropArea;
+        this.area = area;
         this.rendition = GraphicalRendition.DEFAULT;
     }
 
     public DrawArea getSubarea(Position offset, Dimensions dimensions) {
-        return new DrawArea(this.drawBuffer, this.offset.add(offset), dimensions);
+        Rectangle subArea = new Rectangle(this.area.tl().add(Point.from(offset)), dimensions);
+        if (!this.area.contains(subArea)) {
+            throw new IllegalArgumentException("subarea must be enclosed in current drawing area.");
+        }
+
+        Rectangle cropArea = this.cropArea.intersect(subArea).orElse(Rectangle.ZERO);
+
+        return new DrawArea(this.drawBuffer, cropArea, subArea);
+    }
+
+    public DrawArea getRelativeArea(int xOffset, int yOffset, Dimensions dimensions) {
+        Rectangle newArea = new Rectangle(new Point(xOffset, yOffset), dimensions);
+        Rectangle cropArea = this.cropArea.intersect(newArea).orElse(Rectangle.ZERO);
+
+        return new DrawArea(this.drawBuffer, cropArea, newArea);
     }
 
     public Dimensions dimensions() {
-        return this.dimensions;
+        return this.area.dimension();
     }
 
+    // position is specified in DrawArea space
     public void draw(Position position, char c) {
-        this.drawBuffer.draw(position.add(this.offset), this.rendition, c);
+        // 2. transform from coordinates in drawArea space to drawBuffer space
+        Point drawPoint = new Point(this.area.tl().x() + position.x(), this.area.tl().y() + position.y());
+
+        // 1. check if drawing inside draw area
+        if (!this.area.contains(drawPoint)) {
+            return;
+        }
+
+        // 3. apply crop
+        if (!this.cropArea.contains(drawPoint)) {
+            return;
+        }
+
+        // at this point we can be sure that the drawPoint is inside the drawBuffer, ad this x and y are positive,
+        // so we can cover to a Position without problems
+        Position drawPosition = new Position(drawPoint.x(), drawPoint.y());
+
+        this.drawBuffer.draw(drawPosition, this.rendition, c);
     }
 
     public void draw(int x, int y, char c) {
@@ -70,6 +107,57 @@ public class DrawArea {
 
     public void background(GraphicalRenditionProperty.BackgroundColor color) {
         this.rendition = rendition.withBackground(color);
+    }
+
+    // IMPORTANT! Point is different from Position because x and y can be negative
+    protected record Point(int x, int y) {
+        public static final Point ZERO = new Point(0, 0);
+
+        public static Point from(Position position) {
+            return new Point(position.x(), position.y());
+        }
+
+        public Point add(int x, int y) {
+            return new Point(this.x + x, this.y + y);
+        }
+
+        public Point add(Point p) {
+            return new Point(this.x + p.x, this.y + p.y);
+        }
+    }
+
+    protected record Rectangle(Point tl, Dimensions dimension) {
+        public static final Rectangle ZERO = new Rectangle(Point.ZERO, Dimensions.ZERO);
+
+        public boolean contains(Point pos) {
+            return tl.x() <= pos.x() && pos.x() <= tl.x() + dimension.width() &&
+                    tl.y() <= pos.y() && pos.y() <= tl.y() + dimension.height();
+        }
+
+        public boolean contains(Rectangle other) {
+            Point otherBr = other.tl.add(other.dimension.width(), other.dimension.height());
+
+            return this.contains(other.tl) && this.contains(otherBr);
+        }
+
+        public Point br() {
+            return new Point(tl.x() + dimension.width(), tl.y() + dimension.height());
+        }
+
+        public Optional<Rectangle> intersect(Rectangle other) {
+            Point tl = new Point(
+                    Math.max(this.tl.x, other.tl.x),
+                    Math.max(this.tl.y, other.tl.y)
+            );
+
+            int w = Math.min(this.br().x(), other.br().x()) - tl.x;
+            int h = Math.min(this.br().y(), other.br().y()) - tl.y;
+            if (w < 0 || h < 0) {
+                return Optional.empty();
+            }
+
+            return Optional.of(new Rectangle(tl, new Dimensions(w, h)));
+        }
     }
 
 }
