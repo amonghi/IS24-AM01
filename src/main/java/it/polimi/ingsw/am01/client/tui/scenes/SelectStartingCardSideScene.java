a3b40a4a32b808f0324829d7e441e2be847aecd8
Matteo Garzone <matteo.garzone@mail.polimi.it>
2024-06-16 23:28:01 +0200

    fix: starting card choices positioning in tui

--- src/main/java/it/polimi/ingsw/am01/client/tui/scenes/SelectStartingCardSideScene.java
+++ src/main/java/it/polimi/ingsw/am01/client/tui/scenes/SelectStartingCardSideScene.java
@@ -5,10 +5,7 @@ import it.polimi.ingsw.am01.client.tui.component.Component;
 import it.polimi.ingsw.am01.client.tui.component.elements.CardFaceComponent;
 import it.polimi.ingsw.am01.client.tui.component.elements.Composition;
 import it.polimi.ingsw.am01.client.tui.component.elements.Text;
-import it.polimi.ingsw.am01.client.tui.component.layout.Border;
-import it.polimi.ingsw.am01.client.tui.component.layout.Centered;
-import it.polimi.ingsw.am01.client.tui.component.layout.Column;
-import it.polimi.ingsw.am01.client.tui.component.layout.Padding;
+import it.polimi.ingsw.am01.client.tui.component.layout.*;
 import it.polimi.ingsw.am01.client.tui.component.layout.flex.Flex;
 import it.polimi.ingsw.am01.client.tui.component.layout.flex.FlexChild;
 import it.polimi.ingsw.am01.client.tui.rendering.draw.Line;
@@ -50,7 +47,7 @@ public class SelectStartingCardSideScene extends Composition {
                                 )
                         )
                 ),
-                new FlexChild.Flexible(2,
+                new FlexChild.Flexible(1,
                         Flex.row(
                                 Arrays.stream(Side.values())
                                         .map(side -> new FlexChild.Flexible(1,
@@ -72,16 +69,20 @@ public class SelectStartingCardSideScene extends Composition {
                 new FlexChild.Flexible(1,
                         Centered.horizontally(
                                 new Border(Line.Style.DEFAULT,
-                                        new Column(
+                                        new Row(
                                                 view.getStartingCardPlacements().isEmpty() ? List.of(Padding.around(1, new Text("Nobody has chosen yet"))) :
                                                         view.getStartingCardPlacements()
-                                                                .values()
+                                                                .entrySet()
                                                                 .stream()
-                                                                .map(v -> Padding.around(1,
-                                                                        new CardFaceComponent( // FIXME: dimensions
-                                                                                GameAssets.getInstance().getCardById(v.id()).orElseThrow(),
-                                                                                v.side()
+                                                                .map(e -> new Column(List.of(
+                                                                        new Text(e.getKey()),
+                                                                        Padding.around(1,
+                                                                                new CardFaceComponent(
+                                                                                        GameAssets.getInstance().getCardById(e.getValue().id()).orElseThrow(),
+                                                                                        e.getValue().side()
+                                                                                )
                                                                         )
+                                                                )
                                                                 ))
                                                                 .collect(Collectors.toList())
                                         )
