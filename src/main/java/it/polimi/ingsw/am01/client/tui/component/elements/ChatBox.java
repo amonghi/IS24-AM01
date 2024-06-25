8f774057ee30be17aa64d0566dc419538baed5fc
Dario Crosa <dario.crosa@mail.polimi.it>
2024-06-25 13:08:27 +0200

    feat: put messages in chat at the bottom of the ChatBox

--- src/main/java/it/polimi/ingsw/am01/client/tui/component/elements/ChatBox.java
+++ src/main/java/it/polimi/ingsw/am01/client/tui/component/elements/ChatBox.java
@@ -3,10 +3,10 @@ package it.polimi.ingsw.am01.client.tui.component.elements;
 import it.polimi.ingsw.am01.client.View;
 import it.polimi.ingsw.am01.client.tui.TuiView;
 import it.polimi.ingsw.am01.client.tui.component.Component;
-import it.polimi.ingsw.am01.client.tui.component.layout.Border;
-import it.polimi.ingsw.am01.client.tui.component.layout.Centered;
-import it.polimi.ingsw.am01.client.tui.component.layout.Column;
-import it.polimi.ingsw.am01.client.tui.component.layout.Padding;
+import it.polimi.ingsw.am01.client.tui.component.layout.*;
+import it.polimi.ingsw.am01.client.tui.rendering.Constraint;
+import it.polimi.ingsw.am01.client.tui.rendering.Dimensions;
+import it.polimi.ingsw.am01.client.tui.rendering.Position;
 import it.polimi.ingsw.am01.client.tui.rendering.draw.Line;
 
 import java.util.ArrayList;
@@ -14,11 +14,9 @@ import java.util.List;
 
 public class ChatBox extends Composition {
 
-    private final TuiView view;
     private final List<Component> messagesComponents;
 
     public ChatBox(TuiView view) {
-        this.view = view;
         this.messagesComponents = new ArrayList<>();
         for (View.Message m : view.getMessages()) {
             messagesComponents.add(
@@ -34,8 +32,36 @@ public class ChatBox extends Composition {
         return new Border(Line.Style.DEFAULT,
                 new Column(List.of(
                         Centered.horizontally(Padding.hv(0, 1, new Text("Chat"))),
-                        messagesComponents.isEmpty() ? new Text("No messages yet") : new Column(messagesComponents)
+                        messagesComponents.isEmpty()
+                                ? new Text("No messages yet")
+                                : new ChatScrollable(messagesComponents)
                 ))
         );
+    }
+
+    protected static class ChatScrollable extends Layout {
+        public ChatScrollable(List<Component> messagesComponents) {
+            super(messagesComponents.reversed());
+        }
+
+        @Override
+        public void layout(Constraint constraint) {
+            Dimensions d = constraint.max();
+            this.setDimensions(d);
+
+            int y = constraint.max().height();
+
+            for (int i = 0; i < this.children().size() && y >= 0; i++) {
+                Component child = this.children().get(i);
+
+                Constraint availableSpace = Constraint.max(Dimensions.of(constraint.max().width(), y));
+                child.layout(availableSpace);
+
+                int childTop = y - child.dimensions().height();
+                child.setPosition(Position.of(0, childTop));
+
+                y = childTop;
+            }
+        }
     }
 }
